7355d66e2dfee9c7c9ff668123e519f722e66a6f
Alexander Valeriano <alialvale261@gmail.com>
2024-06-10 06:49:26 -0400

    solo un cambio

--- Source/Galaga_USFX_L01/Controlador.h
+++ Source/Galaga_USFX_L01/Controlador.h
@@ -22,5 +22,5 @@ class GALAGA_USFX_L01_API IControlador
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	virtual void Notificar(AActor* _ActorQueNotifica, const FString& _Evento) = 0;
+	virtual void Notificar(AActor* _Emisor, const FString& _Evento) = 0;
 };
